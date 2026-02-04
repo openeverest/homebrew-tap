85b1491e235e7abec098e57b158ac6b4cdb86468
Sergey Pronin <sp@solanica.io>
2026-02-04 14:30:41 +0300

    Formula init

---
+++ Formula/everestctl.rb
@@ -0,0 +1,51 @@
+class Everestctl < Formula
+  desc "CLI tool for provisioning and managing OpenEverest on Kubernetes"
+  homepage "https://github.com/openeverest/openeverest"
+  license "Apache-2.0"
+  version "1.13.0"
+
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/openeverest/openeverest/releases/download/v1.13.0/everestctl-darwin-amd64"
+      sha256 "b1340ad8c1cb67c8cb570943511b3740dc3b5d231510205331ef4d32c990f477"
+    elsif Hardware::CPU.arm?
+      url "https://github.com/openeverest/openeverest/releases/download/v1.13.0/everestctl-darwin-arm64"
+      sha256 "e62b6975ca1e5e5cf24053c73e59f57be8180750718caca43f230093870af9cc"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/openeverest/openeverest/releases/download/v1.13.0/everestctl-linux-amd64"
+      sha256 "edd42d88711ba7e66f763f02d72e34b9f765e9253fcf4e4a7ccb85449734a96c"
+    elsif Hardware::CPU.arm?
+      url "https://github.com/openeverest/openeverest/releases/download/v1.13.0/everestctl-linux-arm64"
+      sha256 "573bbd6f5394ef96be684c5bd3081481b2e47b91e9551cf545bc333df6986253"
+    end
+  end
+
+  def install
+    # Determine which file was downloaded
+    arch = Hardware::CPU.intel? ? "amd64" : "arm64"
+    os = OS.mac? ? "darwin" : "linux"
+    
+    # Rename the downloaded binary to 'everestctl' and install it
+    bin.install "everestctl-#{os}-#{arch}" => "everestctl"
+  end
+
+  def caveats
+    <<~EOS
+      To get started with OpenEverest:
+        everestctl install
+
+      For headless installation:
+        everestctl install --namespaces <namespace> --operator.mongodb=true --operator.postgresql=true --operator.mysql=true --skip-wizard
+
+      Documentation: https://openeverest.io/documentation/current/
+    EOS
+  end
+
+  test do
+    assert_match "everestctl version", shell_output("#{bin}/everestctl --version 2>&1")
+  end
+end
--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# homebrew-tap
-OpenEverest CLI - everestctl - home brew tap
+# OpenEverest Homebrew Tap
+
+Homebrew tap for `everestctl` - the OpenEverest CLI tool for managing OpenEverest on Kubernetes.
+
+## Installation
+
+```bash
+brew tap openeverest/tap
+brew install everestctl
+```
+
+## Usage
+
+### Install OpenEverest
+
+Install with wizard (interactive):
+```bash
+everestctl install
+```
+
+Install without wizard (headless):
+```bash
+everestctl install --namespaces <namespace-name> --operator.mongodb=true --operator.postgresql=true --operator.mysql=true --skip-wizard
+```
+
+### Common Commands
+
+Add a namespace:
+```bash
+everestctl namespaces add <NAMESPACE>
+```
+
+Skip database namespace during installation:
+```bash
+everestctl install --skip-db-namespace
+```
+
+Update admin password:
+```bash
+everestctl accounts set-password --username admin
+```
+
+## Documentation
+
+For detailed installation and usage instructions, see the [everestctl installation documentation](https://openeverest.io/documentation/current).
